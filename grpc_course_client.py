38da4f018a69cd124a7d654c768c478b24a8c064
kokaevao <alexander.kokaev@gmail.com>
2026-03-05 17:07:25 +0800

    Знакомство с gRPC протоколом(HomeWork)

---
+++ grpc_course_client.py
@@ -0,0 +1,10 @@
+import grpc
+import course_service_pb2
+import course_service_pb2_grpc
+
+
+channel = grpc.insecure_channel('localhost:50051')
+stub = course_service_pb2_grpc.CourseServiceStub(channel)
+
+Response = stub.GetCourse(course_service_pb2.GetCourseRequest(course_id="api-course"))
+print(Response)
